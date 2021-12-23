4058be0802a0d3124875d8734ae99ea59eb86f16
semkor <selsasss@gmail.com>
2021-12-23 16:05:01 +0200

    выаыв

---
+++ src/lesson34A/Demo.java
@@ -0,0 +1,7 @@
+package lesson34A;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution.copyFileContent("C:\\Users\\Администратор\\Desktop\\From.txt", "C:\\Users\\Администратор\\Desktop\\To.txt");
+    }
+}
