bfc2dc3f9d6d8b7b6f29763226101cd919b88ac1
semkor <selsasss@gmail.com>
2021-12-24 06:05:16 +0200

    выаыв

---
+++ src/lesson34C/Demo.java
@@ -0,0 +1,7 @@
+package lesson34C;
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution.transferSentences("C:\\Users\\Администратор\\Desktop\\From.txt", "C:\\Users\\Администратор\\Desktop\\To.txt","samson");
+    }
+}
---
+++ src/lesson34C/Solution.java
@@ -0,0 +1,86 @@
+package lesson34C;
+
+import java.io.*;
+
+public class Solution {
+    /*переносит все предложения, которые содержат входящее слово
+            - предложением считаем текст отделеннный точкой(.) длинной больше 10 символов (включая пробелы)
+            - перенесенное предложение при этом должно быть удалено из входящего файла
+
+            //в fileFrom:
+                - находим нужное слово
+                    - вырезаем предложение (от предыдущей точки до следующей точки)
+                        -проверяем предложение больше 10 символов
+                                - нет - идем дальше искать;
+                                - да  - идем дальше искать
+                                        - вырезаем предложение от точки + пробел -предложение с точкой -
+                                        - копируем его в буффер, добавляее при этом перенос
+                                        - удаляем предложение из файла
+                                 - вставляем скопированный буфер в другой файл
+     */
+
+
+    public static void transferSentences(String fileFromPath, String fileToPath,String wordToCheck) {
+        try {
+            validate(fileFromPath, fileToPath);
+        } catch (Exception e) {
+            System.err.println("error: " + e.getMessage());
+            System.exit(0);
+        }
+        writeToFile(fileToPath, readFromFile(fileFromPath), true);
+    }
+
+    private static void validate(String fileFromPath, String fileToPath) throws Exception {
+        File fileFrom = new File(fileFromPath);
+        File fileTo = new File(fileToPath);
+
+        if (!fileFrom.exists()) {
+            throw new FileNotFoundException("File " + fileFrom + " does not exist");
+        }
+        if (!fileTo.exists()) {
+            throw new FileNotFoundException("File " + fileTo + " does not exist");
+        }
+        if (!fileFrom.canRead()) {
+            throw new Exception("File " + fileFrom + " does not have permissions to read");
+        }
+        if (!fileTo.canWrite()) {
+            throw new Exception("File " + fileTo + " does not have permissions to read");
+        }
+    }
+
+    private static StringBuffer readFromFile(String path) {
+        StringBuffer res = new StringBuffer();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            try {
+            String line;
+                while ((line = br.readLine()) != null) {
+                    res.append(line);
+                    res.append("\n");
+                }
+                deleteContentFromFile(path, res);
+            } catch (OutOfMemoryError e) {
+                System.err.println("It is not possible to transfer content ");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            System.err.println("Reading from file" + path + "failed");
+            System.exit(0);
+        }
+        return res;
+    }
+
+    private static void deleteContentFromFile(String path, StringBuffer res) {
+        StringBuffer res2 = new StringBuffer(res);
+        res2.delete(0, res2.length());
+        writeToFile(path, res2, false);
+    }
+
+    private static void writeToFile(String path, StringBuffer contentToWrite, boolean append) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, append))) {
+            bufferedWriter.append(contentToWrite);
+        } catch (IOException e) {
+            System.err.println("Can't write to file");
+            System.exit(0);
+        }
+    }
+}
